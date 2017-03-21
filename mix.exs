e8044169541f8cb09b31edfba4bafa032f441e0b
danny <danny.dev8@gmail.com>
2017-03-21 22:36:12 +0900

    Add package

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule SlackInvitation.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {SlackInvitation, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext]]
+     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext, :httpoison]]
   end
 
   # Specifies which paths to compile per environment.
@@ -33,6 +33,9 @@ defmodule SlackInvitation.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:json, "~> 1.0"},
+     {:httpoison, "~> 0.10.0"}
+    ]
   end
 end
